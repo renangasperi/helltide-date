53561c3cbf8565a9906529cdb00e7166f795dd90
renan gasperi <renangasperi@gmail.com>
2023-06-06 22:58:58 -0300

    added timer and next events ocurrence

--- main.js
+++ main.js
@@ -14,12 +14,25 @@ function verificarEvento(dataHoraAtual) {
       var minutosRestantes = Math.floor(
         (duracaoEvento - tempoDecorrido) / (1000 * 60)
       );
-      return { ocorrendo: true, minutosRestantes: minutosRestantes };
+      var segundosRestantes =
+        Math.floor((duracaoEvento - tempoDecorrido) / 1000) % 60;
+      return {
+        ocorrendo: true,
+        minutosRestantes: minutosRestantes,
+        segundosRestantes: segundosRestantes,
+      };
     } else {
       var minutosProximoEvento = Math.floor(
         (duracaoEvento + intervaloEspera - tempoDecorrido) / (1000 * 60)
       );
-      return { ocorrendo: false, minutosProximoEvento: minutosProximoEvento };
+      var segundosProximoEvento =
+        Math.floor((duracaoEvento + intervaloEspera - tempoDecorrido) / 1000) %
+        60;
+      return {
+        ocorrendo: false,
+        minutosProximoEvento: minutosProximoEvento,
+        segundosProximoEvento: segundosProximoEvento,
+      };
     }
   } else {
     var minutosParaInicio = Math.floor(-diff / (1000 * 60));
@@ -27,24 +40,32 @@ function verificarEvento(dataHoraAtual) {
   }
 }
 
-const dataHoraAtual = new Date();
-const dataHoraEvento = new Date("2023-06-05T07:00:00");
-const duracaoEvento = 60 * 60 * 1000;
-const intervaloEspera = 75 * 60 * 1000;
+function atualizarTempoRestante() {
+  const dataHoraAtual = new Date();
+  const resultado = verificarEvento(dataHoraAtual);
 
-const resultado = verificarEvento(
-  dataHoraAtual,
-  dataHoraEvento,
-  duracaoEvento,
-  intervaloEspera
-);
-
-document.addEventListener("DOMContentLoaded", function () {
-  const tituloElement = document.getElementById("titulo");
-  const statusElement = document.getElementById("status");
   const tempoElement = document.getElementById("tempo");
 
-  tituloElement.textContent = "Maré Infernal";
+  if (resultado.ocorrendo) {
+    const minutosRestantes = resultado.minutosRestantes;
+    const segundosRestantes = resultado.segundosRestantes;
+    tempoElement.textContent = `${minutosRestantes} minutos e ${segundosRestantes} segundos`;
+  } else {
+    const minutosProximoEvento = resultado.minutosProximoEvento;
+    const segundosProximoEvento = resultado.segundosProximoEvento;
+    tempoElement.textContent = `${minutosProximoEvento} minutos e ${segundosProximoEvento} segundos`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const statusElement = document.getElementById("status");
+  const textoElement = document.getElementById("texto");
+  const tempoElement = document.getElementById("tempo");
+
+  const dataHoraAtual = new Date();
+  const dataHoraEvento = new Date("2023-06-05T07:00:00");
+  const duracaoEvento = 60 * 60 * 1000;
+  const intervaloEspera = 75 * 60 * 1000;
 
   const resultado = verificarEvento(
     dataHoraAtual,
@@ -55,11 +76,64 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (resultado.ocorrendo) {
     statusElement.textContent = "O evento está ocorrendo.";
-    tempoElement.textContent =
-      "Tempo restante: " + resultado.minutosRestantes + " minutos.";
+    textoElement.textContent = "Tempo restante: ";
   } else {
     statusElement.textContent = "O evento não está ocorrendo.";
-    tempoElement.textContent =
-      "A Maré Infernal começa em: " + resultado.minutosProximoEvento + " minutos.";
+    textoElement.textContent = "A Maré Infernal começa em: ";
   }
+
+  setInterval(atualizarTempoRestante, 1000);
+  gerarHorariosProximosEventos(resultado);
 });
+
+function gerarHorariosProximosEventos(resultado) {
+  const proximosEventos = [];
+  const duracaoEvento = 60 * 60 * 1000;
+  const intervaloEspera = 75 * 60 * 1000;
+  const dataHoraAtual = new Date();
+  let dataProximoEvento;
+
+  if (resultado.ocorrendo) {
+    console.log(minutosRestantes);
+    const tempoDecorrido =
+      duracaoEvento -
+      (resultado.minutosRestantes * 60 + resultado.segundosRestantes) * 1000;
+    dataProximoEvento = new Date(
+      dataHoraAtual.getTime() + tempoDecorrido + intervaloEspera
+    );
+  } else {
+    const tempoParaInicio =
+      ((resultado.minutosProximoEvento + 1) * 60 +
+        resultado.segundosProximoEvento) *
+      1000;
+    dataProximoEvento = new Date(dataHoraAtual.getTime() + tempoParaInicio);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const proximoEventoFormatado = dataProximoEvento.toLocaleString("pt-BR", {
+      day: "numeric",
+      month: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    proximosEventos.push(proximoEventoFormatado.replace(",", " -"));
+    dataProximoEvento = new Date(
+      dataProximoEvento.getTime() + duracaoEvento + intervaloEspera
+    );
+  }
+
+  criarLista(proximosEventos);
+}
+
+function criarLista(array) {
+  const listaElement = document.getElementById("lista");
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+
+    const liElement = document.createElement("li");
+    liElement.textContent = item;
+
+    listaElement.appendChild(liElement);
+  }
+}
